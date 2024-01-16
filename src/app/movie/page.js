10d0b50ce417286ac6c689828db9a50f4b154c70
kaushik7749 <kaushikyadure4977@gmail.com>
2024-01-16 12:01:12 +0530

    initial commit
    initial commit

---
+++ src/app/movie/page.js
@@ -0,0 +1,39 @@
+import React from "react";
+import MovieCard from "@/app/components/MovieCard";
+import styles from "@/app/styles/common.module.css";
+
+const Movie = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  const url =
+    "https://netflix54.p.rapidapi.com/search/?query=stranger&offset=0&limit_titles=50&limit_suggestions=20&lang=en";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "1490c8af2cmsh5dc480ced88be19p1ac888jsn818750fd9e46",
+      "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
+    },
+  };
+
+  const res = await fetch(url, options);
+  const data = await res.json();
+  const main_data = data.titles;
+  //console.log(main_data.jawSummary);
+
+  return (
+    <>
+      <section className={styles.movieSection}>
+        <div className={styles.container}>
+          <h1>Series & Movie</h1>
+          <div className={styles.card_section}>
+            {main_data?.map((curElem) => {
+              return <MovieCard key={curElem.id} {...curElem} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Movie;
